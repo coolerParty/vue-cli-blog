262f1e3b081a90e8613fd99936896ec0b89ee8d5
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-08-28 15:57:32 +0800

    Component trial

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <cat-card
+      v-for="item in listOfCats"
+      v-bind:key="item.id"
+      :title="item.title"
+      :msg="item.msg"
+      :link="item.link"
+    />
+  </div>
+</template>
+
+<script>
+import CatCard from "./components/CatCard.vue";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      listOfCats: [
+        {
+          id: 1,
+          title: "Cats",
+          msg: "Welcome to Your Vue.js App",
+          link: "www.cats.com",
+        },
+        {
+          id: 2,
+          title: "Big Cats",
+          msg: "Welcome to Your Vue.js App",
+          link: "www.cats.com",
+        },
+        {
+          id: 3,
+          title: "Bigger Cats",
+          msg: "Welcome to Your Vue.js App",
+          link: "www.cats.com",
+        },
+      ],
+    };
+  },
+  components: {
+    CatCard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CatCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="CatCard">
+    <h1>{{ title }}</h1>
+    <p>{{ msg }}</p>
+    <a :href="link">{{ link }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CatCard",  
+  props: {
+    msg: String,
+    title: String,
+    link: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
