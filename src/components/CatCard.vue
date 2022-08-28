c3d675c71f1943cad66c9e11e226ccea20a557b3
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-08-28 17:22:14 +0800

    Using URL & image Binding

--- src/components/CatCard.vue
+++ src/components/CatCard.vue
@@ -1,21 +1,23 @@
 <template>
-  <div :class="[cat.visible ? 'visible' : 'invisible', 'catCard']">
-    <div v-if="cat.visible == true">
-      <h2>{{ cat.id }}</h2>
-      <h1>
-        {{ cat.title }}
-      </h1>
-      <p>{{ cat.msg }}</p>
-      <a :href="cat.link" target="_blank">{{ cat.link }}</a>
+  <a :href="cat.link">
+    <div :class="[cat.visible ? 'visible' : 'invisible', 'catCard']">
+      <div v-if="cat.visible == true">
+      <img :src="cat.image" alt="" class="catImage">
+        <h1>
+          {{ cat.title }}
+        </h1>
+        <p>{{ cat.msg }}</p>
+        <a :href="cat.link" target="_blank">{{ cat.link }}</a>
+      </div>
+      <div v-else-if="cat.id == 3">
+        <img :src="cat.image" alt="" class="catImage">
+        <h1>{{ cat.title }}</h1>
+      </div>
+      <div v-else>
+        <strong>{{ cat.id }} This is not available.</strong>
+      </div>
     </div>
-    <div v-else-if="cat.id == 3">
-      <h2>{{ cat.id }}</h2>
-      <h1>{{ cat.title }}</h1>
-    </div>
-    <div v-else>
-      <strong>{{ cat.id }} This is not available.</strong>
-    </div>
-  </div>
+  </a>
 </template>
 
 <script>
@@ -29,6 +31,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  a {
+    text-decoration: none;
+    color: white;
+  }
 .catCard {
   display: inline-flex;
   border: 1px solid #e6e6e6;
@@ -41,5 +47,10 @@ export default {
 }
 .invisible {
   background: grey;
+}
+.catImage{
+  width: 100%;
+  height: 100px;
+  object-fit: cover;
 }
 </style>
